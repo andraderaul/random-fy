ecba47bec3d831d518ef8d60434daa4ab7cecfd7
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-07 23:09:14 -0300

    fix: header on mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,15 +20,15 @@ export const Header = ({ cookies = CookiesUtils }: HeaderProps) => {
         <Title>Randomfy</Title>
       </div>
 
-      <header className="flex flex-col">
-        <nav className="text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
+      <header className="flex sm:flex-col flex-row justify-between">
+        <nav className="p-1 sm:p-0 text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
           <Link href="/">Home</Link>
         </nav>
-        <nav className="text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
+        <nav className="p-1 sm:p-0 text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
           <Link href="/about">About</Link>
         </nav>
         {isAuthenticated && (
-          <nav className="text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
+          <nav className="p-1 sm:p-0 text-2xl font-thin dark:text-gray-300 text-gray-700 leading-10 tracking-wide">
             <Link href="/logout">Log out</Link>
           </nav>
         )}
