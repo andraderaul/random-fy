bb893393e5c9f5ed1cd6b78d6c6726d742487088
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-09 21:43:37 -0300

    fix: remove refetch on window focus

--- src/queries/use-recommendation/use-recommendation.ts
+++ src/queries/use-recommendation/use-recommendation.ts
@@ -6,6 +6,9 @@ import { Recommendation } from 'types'
 export const useRecommendation = (id: string, trackId = '') => {
   return useQuery<AxiosResponse<Array<Recommendation>>, AxiosError>(
     ['recomendations', id, trackId],
-    () => getRecomendations(id)
+    () => getRecomendations(id),
+    {
+      refetchOnWindowFocus: false
+    }
   )
 }
