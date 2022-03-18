9255bdd1354e27733ad36c1429442f1072d18338
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-18 20:26:50 -0300

    fix: footer position

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,12 @@
 export const Footer = () => {
   return (
-    <footer className="pb-6 flex justify-center font-thin dark:text-gray-300 text-gray-700">
+    <footer
+      className="p-4 flex 
+      justify-center font-thin dark:text-gray-300 
+      text-gray-700 fixed bottom-0 left-0 right-0
+      bg-neutral-50 dark:bg-neutral-700
+      "
+    >
       <p>
         {`Created with `}
         <span className="text-rose-600">♥️</span>
