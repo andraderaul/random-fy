03b9dbd07e7544d06ff88439c7f88709939ae833
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-18 21:05:35 -0300

    fix: play icon size in the image box

--- src/components/audio-player/audio-player.tsx
+++ src/components/audio-player/audio-player.tsx
@@ -49,7 +49,7 @@ export const AudioPlayer = ({
       {state.playing ? (
         <PauseIcon
           aria-label="pause-icon"
-          className="h-20 w-20 sm:h-10 sm:w-10 dark:fill-gray-100 fill-gray-600 cursor-pointer
+          className="h-14 w-14 sm:h-10 sm:w-10 dark:fill-gray-100 fill-gray-600 cursor-pointer
           hover:scale-110
           transition duration-200 ease-out hover:ease-in"
           onClick={pause}
