1f824030cce9d2e435d6715f77f04a9148a99d11
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-19 20:41:47 -0300

    fix: improve title colors

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -4,8 +4,16 @@ type TitleProps = Record<string, unknown>
 
 export const Title = ({ children }: PropsWithChildren<TitleProps>) => {
   return (
-    <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 dark:before:bg-gray-600 before:bg-gray-200 relative inline-block">
-      <h1 className="relative text-5xl sm:text-7xl text-gray-800 dark:text-gray-200 font-semibold">
+    <span
+      className="before:block before:absolute 
+        before:-inset-1 before:-skew-y-3 dark:before:bg-gray-600
+        before:bg-gray-200 relative inline-block"
+    >
+      <h1
+        className="relative text-5xl sm:text-7xl 
+          text-gray-800 dark:text-gray-200 font-semibold
+          hover:text-blue-500 hover:dark:text-orange-400 transition-all duration-700"
+      >
         {children}
       </h1>
     </span>
