b72be6ade2759ebbb1bfadaa7f654742f6c815a2
Raul Andrade <raul.andrade@mercadofavo.com>
2022-03-19 22:03:48 -0300

    wip

--- src/features/festival/festival.tsx
+++ src/features/festival/festival.tsx
@@ -1,4 +1,5 @@
 import { Title } from 'components'
+import Image from 'next/image'
 import { Recommendation } from 'types'
 
 type FestivalProps = {
@@ -12,50 +13,56 @@ export const Festival = ({ artists }: FestivalProps) => {
     <div
       className="min-h-96 p-10 flex flex-col
       justify-center items-center uppercase
-      bg-gradient-to-t from-amber-200 via-yellow-50 to-amber-200
-      text-gray-800 relative"
+      bg-gradient-to-b from-neutral-700 via-neutral-500 to-neutral-700
+      text-gray-800 dark:text-gray-200 relative
+      overflow-hidden
+      "
     >
-      <div className="text-7xl p-10 font-extrabold text-red-500">
-        <span>randomfest 2022</span>
+      <div className="z-10 mb-32">
+        <div className="text-7xl p-10 mb-32 font-extrabold text-red-500">
+          <span>randomfest 2022</span>
+        </div>
+
+        <div className="text-9xl p-2 text-center">
+          {artistsName.slice(0, 1).map((artist) => (
+            <span className="p-2" key={artist}>
+              {artist}
+            </span>
+          ))}
+        </div>
+        <div className="text-7xl flex flex-wrap justify-center">
+          {artistsName.slice(1, 3).map((artist) => (
+            <span className="p-2" key={artist}>
+              {artist}
+            </span>
+          ))}
+        </div>
+        <div className="text-4xl flex flex-wrap justify-center">
+          {artistsName.slice(3, 6).map((artist) => (
+            <span className="p-2" key={artist}>
+              {artist}
+            </span>
+          ))}
+        </div>
+        <div className="text-xl flex flex-wrap justify-center">
+          {artistsName.slice(6).map((artist) => (
+            <span className="p-2" key={artist}>
+              {artist}
+            </span>
+          ))}
+        </div>
       </div>
 
-      <div className="text-9xl p-2">
-        {artistsName.slice(0, 1).map((artist) => (
-          <span className="p-2" key={artist}>
-            {artist}
-          </span>
-        ))}
-      </div>
-      <div className="text-7xl flex flex-wrap justify-center">
-        {artistsName.slice(1, 3).map((artist) => (
-          <span className="p-2" key={artist}>
-            {artist}
-          </span>
-        ))}
-      </div>
-      <div className="text-4xl flex flex-wrap justify-center">
-        {artistsName.slice(3, 6).map((artist) => (
-          <span className="p-2" key={artist}>
-            {artist}
-          </span>
-        ))}
-      </div>
-      <div className="text-xl flex flex-wrap justify-center">
-        {artistsName.slice(6).map((artist) => (
-          <span className="p-2" key={artist}>
-            {artist}
-          </span>
-        ))}
-      </div>
+      <img className="absolute bottom-0 opacity-80" src="/img/bg.jpg" />
 
-      <div
+      {/* <div
         className="
         min-h-96
         opacity-10
         absolute inset-0
-        bg-[url('https://cdn.dribbble.com/users/1314444/screenshots/6129776/media/2365faf88f9faa6a211da550d8b04e89.png?compress=1&resize=800x600&vertical=top')]
+        bg-[url('/img/bg.jpg')]
         z-10"
-      />
+      /> */}
     </div>
   )
 }
