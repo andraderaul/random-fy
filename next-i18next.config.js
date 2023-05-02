9ff497678a372123b3b3b9040dcc2847af0c8706
Raul Andrade <raul10.3@hotmail.com>
2023-05-02 08:57:20 -0300

    fix: ssr i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'pt']
+    locales: ['en', 'pt'],
+    localePath: path.resolve('./public/locales')
+  },
+  fallbackLng: {
+    default: ['en'],
+    'en-US': ['en']
   }
 }
